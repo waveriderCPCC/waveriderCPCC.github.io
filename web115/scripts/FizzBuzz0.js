3d2cf6cbdee0c5f267ad811bcd0daf7dc4f996a4
waverider <unstinkable919@gmail.com>
2025-09-18 18:01:07 -0400

    fizz buzz 0

---
+++ web115/scripts/FizzBuzz0.js
@@ -0,0 +1,40 @@
+function useForm(event) {
+    event.preventDefault(); // this stops the page from refreshing.
+    const first = document.getElementById("first");
+    const mid = document.getElementById("mid");
+    const last = document.getElementById("last");
+    console.log(first, mid, last);
+    if (first.value==="" || last.value==="") {
+        console.log("form is empty. exiting...");
+        return true;
+    } else {
+        const greets = document.getElementById("greets");
+        greets.innerText = "welcome to waverider's site, "+first.value;
+        if (!(mid.value==="")) {
+            greets.innerText += " "+mid.value+".";
+        }
+        greets.innerText += " "+last.value+"!";
+        console.log("updated title.");
+        console.log(greets.innerText);
+        var amt = prompt("How high do you want to count, "+first.value+"?");
+        var dd = document.getElementById("datas");
+        var val = "";
+        for (let i=0; i<amt; i++) {
+            if (i % 2 === 0) {
+                val="even";
+            } else {
+                val="odd";
+            }
+            dd.innerHTML = dd.innerHTML+
+            "<li><span class=\"fc\">fun code</span>"
+            +" - "
+            +"the number is "
+            +val
+            +"</li>";
+        }
+        return false;
+    }
+}
+
+var form = document.getElementById("form");
+form.addEventListener('submit', useForm);
